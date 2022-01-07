33d8daf873f5bc56f85c5bb52fee340809d062f8
Bruno <brunocsofiatti@gmail.com>
2022-01-07 17:43:08 -0300

    Adicionando o material-table-django admin

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from django.contrib.admin import ModelAdmin, register
 
-# Register your models here.
+from persons.models import Person
+
+@register(Person)
+class PersonAdmin(ModelAdmin):
+    list_display = ('name', 'first_name', 'last_name', 'age')
+    name = 'persons'
+    icon_name = 'person_pin'
